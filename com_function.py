2b0327bbf5b05712d64fd535c99b176a1b4eb0ad
SmrutiPatil <patilsmrutim248@gmail.com>
2023-04-23 18:34:59 +0530

    added files

---
+++ com_function.py
@@ -0,0 +1,34 @@
+import mysql.connector
+import os
+from dotenv import load_dotenv
+import tkinter as tk
+
+load_dotenv()
+
+#function to connect with database
+def connect():
+    mydb = mysql.connector.connect( host= "localhost",
+                                user= os.getenv("DB_USER"),
+                                password= os.getenv("DB_PASSWORD"),
+                                database = os.getenv("DB_NAME")
+    )
+    
+    return mydb.cursor()
+
+
+#function to create a warning window   
+def warningWindow(message):
+    root = tk.Tk()
+    frame1 = tk.Frame(root)
+    root.title("Warning")
+    root.geometry('200x75')
+    
+    label= tk.Label(frame1,text=message)
+    label.pack()
+    cancel = tk.Button(frame1,text="Cancel",command=root.destroy)
+    cancel.pack()
+    frame1.pack()
+    
+    root.mainloop()
+    
+    
--- loginRegister.py
+++ loginRegister.py
@@ -1,43 +1,15 @@
-import mysql.connector
-import os
-from dotenv import load_dotenv
-import tkinter as tk
-
-load_dotenv()
-
-mydb = mysql.connector.connect( host= "localhost",
-                               user= os.getenv("DB_USER"),
-                               password= os.getenv("DB_PASSWORD"),
-                               database = os.getenv("DB_NAME")
-)
-
-dbCursor = mydb.cursor()
+import com_function as cf
 
 
 def validateUser(role, id):
+    dbCursor = cf.connect()
     dbCursor.execute(f"""SELECT * FROM {role} WHERE {role}_number = '{id}'""")
     user = dbCursor.fetchone()
     if user:
         return True
     else:
-        warningWindow("Login failed")
+        cf.warningWindow("Login failed")
         return False
     
-# def registerStaff(id):
-    
-    
-def warningWindow(message):
-    root = tk.Tk()
-    frame1 = tk.Frame(root)
-    root.title("Warning")
-    root.geometry('200x75')
-    
-    label= tk.Label(frame1,text=message)
-    label.pack()
-    cancel = tk.Button(frame1,text="Cancel",command=root.destroy)
-    cancel.pack()
-    frame1.pack()
-    
-    root.mainloop()
-    
+def registerStaff(id):
     
