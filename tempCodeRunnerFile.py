2f6a46f6e235dc8c2d079cc97542cbda8dd96bf1
Aayushi Sinha <aayushi.sinha1980@gmail.com>
2023-04-23 12:50:19 +0530

    Added staff and client dashboards

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,8 @@
-            # staff_heading=tk.Frame(staff_reg_window)
-            # staff_heading.grid(row=0, column=0, columnspan=3)
+ Buttons
+        staff_dash_btns=tk.Frame(staff_dash_window)
+        staff_dash_btns.grid(row=1, column=0)
 
-            # staff_heading_title = tk.Label(staff_heading, text="Staff Registration Form", font=("Helvetica", 15), bg="#614051", fg="white")
-            # staff_heading_title.pack(pady=20)
+        staff_listing_btn = tk.Button(staff_dash_btns, text="View Staff Listing", command=staffList)
+        staff_listing_btn.grid(row=0, column=0, padx=20, pady=20)
+        property_listing_btn = tk.Button(staff_dash_btns, text="View Property Registered")
+        property_listing_btn.grid(row=0, column=1, padx=20, pady=20)
