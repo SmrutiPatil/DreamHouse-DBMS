d931572ad4b8d6de3f18444e1855281c2a57fc8c
SmrutiPatil <patilsmrutim248@gmail.com>
2023-04-24 09:40:06 +0530

    query files

--- Query Sheet 2.sql
+++ Query Sheet 2.sql
@@ -33,41 +33,43 @@ where Registered_At_Branch="B003" and staff_name="John Doe";
  -- - done till here
  
  #Identify properties located in Seattle with rents no higher than £450.
- select p.property_number,Pstreet,PCity,PPincode,type,rent,managed_by,owner_num,registered_at_branch,BCity
- from property p,branch b 
- where p.Registered_At_Branch=b.Branch_Number
- and b.BCity="Seattle" and rent<45000;
+select p.property_number,Pstreet,PCity,PPincode,type,rent,managed_by,owner_num,registered_at_branch,BCity
+from property p,branch b 
+where p.Registered_At_Branch=b.Branch_Number and b.BCity="New York" and rent<1500;
  
  
- # Identify the name and telephone number of an owner of a given property.
+# Identify the name and telephone number of an owner of a given property.
 select property_number,PStreet,PCity,owner_num,Personal_or_Business_name,Telephone_number
-from property p,owner o 
-where p.Owner_Num=o.Owner_Number;
+from property p
+join owner o on owner_num = owner_number;
 
 #List the details of comments made by clients viewing a given property.
-select * from property_report where Property_Number="3";
+select * from property_report where Property_Number="P0003";
 
 # Display the names and phone numbers of clients who have viewed a given property but not supplied comments.
-select Full_Name,c.Client_Number from client c 
-where c.Client_Number 
-not in (select Client_Number from property_report);
+select Full_Name,c.Client_Number 
+from client c 
+where c.Client_Number in (select Client_Number from property_report where comment=null);
 
 # Display the details of a lease between a named client and a given property.
 select lease_number,l.property_number,full_name
 from property p, lease l,client c 
-where p.Property_Number="4"
-and p.Registered_At_Branch=c.Branch_Number
-and c.Client_Number=l.Client_Number;
+where p.Property_Number="P0004" and c.Client_Number=l.Client_Number and full_name = "Sophia Lee";
 
 #Identify the leases due to expire next month at the branch.
+Select *
+from lease natural join property
+where rent_finish between curdate() and curdate()+1 and registered_at_branch = "B001";
 
 #List the details of properties that have not been rented out for more than three months.
+Select *
+from property
+where abs(timestampdiff(MONTH,curdate(),last_rented_out)) > 3;
 
 #Produce a list of clients whose preferences match a particular property.
 select distinct client_number,full_name,Ptype 
 from property p,client c
-where c.PType=p.Type
-and c.Max_PRent<=p.Rent;
+where c.PType=p.Type and c.Max_PRent<=p.Rent;
 
 
 
